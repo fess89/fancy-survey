220b542ba6165d91ceaffec7d61bd2d51cdeb3b5
fess89 <fess89@gmail.com>
2013-07-08 20:23:50 +0300

    Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,35 @@
 class UsersController < ApplicationController
+
   def index
+  end
+
+  def new
   	@user = User.new
-  	@response = Response.new
   end
 
   def create
   	@user = User.new(params[:user])
-  		if @user.save
-		render :json => @user
+    if @user.save
+      render :create
   	else
-  		head 422
-  	end
+      head 422
+    end
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+    if @user
+      render :edit
+    else
+      head 422
+    end
+  end
+
+  def update
+    @user = User.find_by_id(params[:id])
+  end
+
+  def answer
   end
 
 end
